d4c4426c77c6121c0d8c87cfdb77e0bbb19a1c50
gFelicio <gustavo.a.felicio@gmail.com>
2018-02-20 17:56:38 -0300

    alter artigos table with categoria_id

---
+++ database/migrations/2018_02_20_173415_alter_artigos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterArtigosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('artigos', function(Blueprint $table) {
+            $table->integer('categoria_id')->unsigned()->nullable();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('artigos', function (Blueprint $table) {
+            $table->dropForeign('categoria_id');
+        });
+    }
+}
