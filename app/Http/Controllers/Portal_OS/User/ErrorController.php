6772a22cb0b6ad00e1830ea95165eac0ddc61ef0
Jorge <jorge.fernando.brito@gmail.com>
2018-02-28 14:20:51 -0300

    create error controller

---
+++ app/Http/Controllers/Portal_OS/User/ErrorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+use App\Helpers\ControllerHelper;
+
+use App\Artigo;
+use App\Categoria;
+use App\Seo;
+use App\Media;
+use App\MediaDerivative;
+use Carbon\Carbon;
+
+class ErrorController extends Controller
+{
+    public function pagina404() {
+        return view('Portal_OS.pages.error404');
+    }
+}
