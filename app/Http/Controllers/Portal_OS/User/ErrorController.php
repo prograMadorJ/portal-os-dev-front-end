c8007b5fa204bac262752d39f53a0786579792ce
Jorge <jorge.fernando.brito@gmail.com>
2018-02-28 14:24:04 -0300

    discarded unnecessary code lines

--- app/Http/Controllers/Portal_OS/User/ErrorController.php
+++ app/Http/Controllers/Portal_OS/User/ErrorController.php
@@ -2,18 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Storage;
-
-use App\Helpers\ControllerHelper;
-
-use App\Artigo;
-use App\Categoria;
-use App\Seo;
-use App\Media;
-use App\MediaDerivative;
-use Carbon\Carbon;
 
 class ErrorController extends Controller
 {
