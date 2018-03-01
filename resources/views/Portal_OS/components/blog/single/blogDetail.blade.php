f40cf064dbe033ac4bf467edda4337583d678d68
Jorge <jorge.fernando.brito@gmail.com>
2018-03-01 09:35:49 -0300

    add blade blogDetail

---
+++ resources/views/Portal_OS/components/blog/single/blogDetail.blade.php
@@ -0,0 +1,52 @@
+<div class="blog-detail">
+
+    <div class="blog-detail__tag">
+        <span class="tag__class">
+            TAG ÚNICA
+        </span>
+
+        <span class="tag__timestamp">
+            DD/MM/AA - HH:MM
+        </span>
+    </div>
+
+    <div class="blog-detail__title">
+        <h1>
+            Título do post gigante para testar quebra de linha e essas paradas todas lalala lala lala
+        </h1>
+
+        <span>
+            Texto de preview, sub-título, algo assim. Deve ser gigante também para testar quebra de linha e essas coisas tudo lalala lalala lala
+        </span>
+    </div>
+
+    <div class="blog-detail__image">
+        <img src="{{ asset('img/default.jpg') }}">
+    </div>
+
+    <div class="blog-detail__body">
+        <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </span>
+    </div>
+
+    {{--@include('components.blog.single.components.share')--}}
+
+    {{--@include('components.blog.single.components.postSuggestion')--}}
+
+    {{--@include('components.blog.single.components.multiTag')--}}
+
+    {{--@include('components.blog.single.components.showMoreNews')--}}
+</div>
