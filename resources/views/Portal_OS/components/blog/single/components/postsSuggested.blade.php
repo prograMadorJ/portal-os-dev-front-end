768ece79942a73f6cf684488f47bbfaec4d6adc8
Jorge <jorge.fernando.brito@gmail.com>
2018-03-02 14:26:40 -0300

    code refactoring

--- resources/views/Portal_OS/components/blog/single/components/postsSuggested.blade.php
+++ resources/views/Portal_OS/components/blog/single/components/postsSuggested.blade.php
@@ -1,18 +1,26 @@
 <div class="posts-suggested">
-    <div class="posts-suggested__content">
-        <div class="posts-suggested__title">
-            <p>
-                Você também pode gostar:
-            </p>
-        </div>
-        <div class="posts-suggested__panel">
-            @php
-                $panels = 4;
-            @endphp
-
-            @for($i = 0; $i < $panels; $i++)
-                @include('components.blog.single.components.postSuggestionPanel')
-            @endfor
-        </div>
+    <div class="posts-suggested__title">
+        <h4>
+            VOCÊ TAMBÉM PODE GOSTAR:
+        </h4>
     </div>
+    @php
+        $panels = 4;
+    @endphp
+    @for($i = 0; $i < $panels; $i++)
+        <div class="posts-suggested__panel">
+            <div class="posts-suggested__panel__image">
+                <img src="{{ asset('portal-os/img/img-post.jpg') }}">
+            </div>
+            <div class="posts-suggested__panel__tag">
+                <p>Saúde</p>
+                <span>Há 1 hora</span>
+            </div>
+            <div class="posts-suggested__panel__post">
+                <a href="">
+                    Hábitos alimentares afetam audição
+                </a>
+            </div>
+        </div>
+    @endfor
 </div>
