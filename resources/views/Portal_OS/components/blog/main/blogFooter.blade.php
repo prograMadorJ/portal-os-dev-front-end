d58356076e0aacd728a81af3e08c2def701f0853
Jorge <jorge.fernando.brito@gmail.com>
2018-03-05 14:12:55 -0300

    fix code

--- resources/views/Portal_OS/components/blog/main/blogFooter.blade.php
+++ resources/views/Portal_OS/components/blog/main/blogFooter.blade.php
@@ -2,21 +2,22 @@
     <h4>
         Leia por categoria
     </h4>
-@foreach($categorias as $categoria)
     <div class="blog-footer__items">
-        <div class="blog-footer__items--category">
-            @include('Portal_OS.components.graphics.'.$categoria->slug)
-            <span>
+        @foreach($categorias as $categoria)
+
+            <div class="blog-footer__items--category">
+                @include('Portal_OS.components.graphics.'.$categoria->slug)
+                <span>
                 {{$categoria->nome}}
             </span>
-            <p>
-                {{$categoria->descricao}}
-            </p>
+                <p>
+                    {{$categoria->descricao}}
+                </p>
 
-            <a href="#ler-mais">
-                LER MAIS
-            </a>
-        </div>
+                <a href="#ler-mais">
+                    LER MAIS
+                </a>
+            </div>
+        @endforeach
     </div>
-@endforeach
 </div>
