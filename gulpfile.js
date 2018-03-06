55f07d9feb7f33de22b4b66a03b05586c38e0fbf
Jorge <jorge.fernando.brito@gmail.com>
2018-03-06 15:22:00 -0300

    added gulp dependencies gulp-uglify plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,61 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
+    minify_js = require('gulp-uglify'),
     browserify = require('gulp-browserify');
-    image = require('gulp-image');
+image = require('gulp-image');
 
 var src_dir = 'resources/assets/Portal_OS';
 var dest_dir = 'public/portal-os';
 
 gulp.task('img-minify', function () {
-    gulp.src(dest_dir+'/temp/*')
+    gulp.src(dest_dir + '/temp/*')
         .pipe(image())
         .on('error', swallowError)
-        .pipe(gulp.dest(dest_dir+'/img'));
+        .pipe(gulp.dest(dest_dir + '/img'));
 });
-gulp.task('sass', function(){
-    return gulp.src(src_dir+'/sass/pages/*.scss')
+gulp.task('sass', function () {
+    return gulp.src(src_dir + '/sass/pages/*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(dest_dir+'/css/pages'))
+        .pipe(gulp.dest(dest_dir + '/css/pages'))
 });
-gulp.task('js', function() {
+gulp.task('js', function () {
     // Single entry point to browserify
     gulp.src([
-        src_dir+'/js/pages/*.js'
+        src_dir + '/js/pages/*.js'
     ])
-    // .pipe(browserify())
-        .pipe(gulp.dest(dest_dir+'/js/pages'))
+        .pipe(minify_js())
+        // .pipe(browserify())
+        .pipe(gulp.dest(dest_dir + '/js/pages'))
 });
-gulp.task('dev', function(){
-    gulp.src(src_dir+'sass/pages/*.scss')
+gulp.task('dev', function () {
+    gulp.src(src_dir + 'sass/pages/*.scss')
         .pipe(sass())
         .on('error', swallowError)
-        .pipe(gulp.dest(dest_dir+'/css/pages'));
+        .pipe(gulp.dest(dest_dir + '/css/pages'));
     gulp.src([
-        src_dir+'/js/pages/*.js'
+        src_dir + '/js/pages/*.js'
     ])
         .pipe(browserify({
-            insertGlobals : true,
-            debug : true
+            insertGlobals: true,
+            debug: true
         }))
         .on('error', swallowError)
-        .pipe(gulp.dest(dest_dir+'/js/pages'))
+        .pipe(gulp.dest(dest_dir + '/js/pages'))
 });
-gulp.task('dev-w',['sass','js'], function(){
+gulp.task('dev-w', ['sass', 'js'], function () {
     return gulp.watch([
-        src_dir+'/sass/*/*.scss',
-        src_dir+'/sass/*.scss',
-        src_dir+'/js/*/*.js',
-        src_dir+'/js/*.js'
-    ], ['sass','js']);
+        src_dir + '/sass/*/*.scss',
+        src_dir + '/sass/*.scss',
+        src_dir + '/js/*/*.js',
+        src_dir + '/js/*.js'
+    ], ['sass', 'js']);
 });
-function swallowError (error) {
+
+function swallowError(error) {
     // If you want details of the error in the console
     console.log(error.toString());
     this.emit('end')
