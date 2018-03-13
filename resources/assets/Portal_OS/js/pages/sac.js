83494a471f134db6c9c3c43254d23c54905738f1
Jorge <jorge.fernando.brito@gmail.com>
2018-03-13 10:23:52 -0300

    moved scripts from blade sac

--- resources/assets/Portal_OS/js/pages/sac.js
+++ resources/assets/Portal_OS/js/pages/sac.js
@@ -1,3 +1,14 @@
-form.textField('#name');
-form.phoneField('#phone');
-form.cityStateField('#city');
+form.textField('#nome');
+form.phoneField('#telefone');
+$.event('#select-estado', 'change', function() {
+    estadoId = $("#select-estado").value;
+
+    HttpRequest.get($.route('#select-estado')+'?estadoId='+estadoId, function(res){
+        $.removeAll('.select-cidade');
+
+        for(i = 0; i < (res.data).length; i++) {
+            opt = '<option value="'+res.data[i].id_cidade+'">'+res.data[i].descricao+'</option>';
+            $.append('.select-cidade', opt);
+        }
+    });
+});
